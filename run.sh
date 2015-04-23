0ab3d76c83d2d6ee0679ea03fcf7a45af99e54cd
Qihong Xu <qihongxu@bu.edu>
2015-04-22 21:55:54 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 ImageName="webapp"
-hostIP=8080
+hostIP=1234
 
 #Defaults requiretty
 
 sudo docker -d &
 sleep 5
-cd /
-sudo docker build -t=$ImageName --no-cache vagrant
+sudo docker build -t=$ImageName --no-cache ./
 #sudo docker build -t="ImageName" docker-sample
 sudo docker run -p=\"$hostIP:80\" $ImageName 
-cd /vagrant
